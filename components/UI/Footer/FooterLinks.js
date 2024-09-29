cddf2bb9ffaf6dcc04c68a171e7ffca572fdefd3
Gurpreet <dhoat30@gmail.com>
2024-09-30 09:32:38 +1300

    removed webduel links

--- components/UI/Footer/FooterLinks.js
+++ components/UI/Footer/FooterLinks.js
@@ -1,39 +1,15 @@
 export const footerLinks = [
     {
         id: 2,
-        label: "Service Packages",
-        url: "/service-packages"
+        label: "Services",
+        url: "/services"
     },
     {
         id: 6,
         label: "Get a Quote",
-        url: "/website-enquiry"
+        url: "/get-a-quote"
     },
-    {
-        id: 2,
-        label: "Our Work",
-        url: "/our-work"
-    },
-    {
-        id: 2,
-        label: "About Us",
-        url: "/about-us"
-    },
-    {
-        id: 2,
-        label: "Blogs",
-        url: "/blogs"
-    },
-    {
-        id: 5,
-        label: "Test Website Speed",
-        url: "https://speed.epiccleaning.co.nz"
-    },
-    {
-        id: 5,
-        label: "Book Free Consultation",
-        url: "/book-consultation"
-    },
+
     {
         id: 6,
         label: "Contact",
