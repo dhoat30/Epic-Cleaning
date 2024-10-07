e56887f3747c705ffe89bed13c05689d2e1a8e6e
Gurpreet <dhoat30@gmail.com>
2024-10-07 14:44:00 +1300

    added sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -15,16 +15,38 @@ const getBlogsData = async () => {
 };
 
 
-// const getProjectsData = async () => {
-//     try {
-//         const fetchData = await fetch('https://cms.epiccleaning.co.nz/wp-json/wp/v2/work?acf_format=standard&per_page=100');
-//         const data = await fetchData.json();
-//         return data.map(post => `/our-work/${post.slug}`);
-//     } catch (error) {
-//         console.error('Failed to fetch our work data:', error);
-//         return []; // Return an empty array on error to avoid breaking the sitemap generation
-//     }
-// };
+const getResidentialServices = async () => {
+    try {
+        const fetchData = await fetch('https://cms.epiccleaning.co.nz/wp-json/wp/v2/residential-cleaning?acf_format=standard&per_page=100');
+        const data = await fetchData.json();
+        return data.map(post => `/residential-cleaning/${post.slug}`);
+    } catch (error) {
+        console.error('Failed to fetch our work data:', error);
+        return []; // Return an empty array on error to avoid breaking the sitemap generation
+    }
+};
+
+
+const getCommercialServices = async () => {
+    try {
+        const fetchData = await fetch('https://cms.epiccleaning.co.nz/wp-json/wp/v2/commercial-cleaning?acf_format=standard&per_page=100');
+        const data = await fetchData.json();
+        return data.map(post => `/commercial-cleaning/${post.slug}`);
+    } catch (error) {
+        console.error('Failed to fetch our work data:', error);
+        return []; // Return an empty array on error to avoid breaking the sitemap generation
+    }
+};
+const getIndustrialServices = async () => {
+    try {
+        const fetchData = await fetch('https://cms.epiccleaning.co.nz/wp-json/wp/v2/industrial-cleaning?acf_format=standard&per_page=100');
+        const data = await fetchData.json();
+        return data.map(post => `/industrial-cleaning/${post.slug}`);
+    } catch (error) {
+        console.error('Failed to fetch our work data:', error);
+        return []; // Return an empty array on error to avoid breaking the sitemap generation
+    }
+};
 
 
 module.exports = {
@@ -33,7 +55,9 @@ module.exports = {
     sitemapSize: 1000,
     additionalPaths: async (config) => {
         const blogUrls = await getBlogsData();
-        // const projectUrls = await getProjectsData();
+        const residentialCleaning = await getResidentialServices();
+        const commercialCleaning = await getCommercialServices();
+        const industrialCleaning = await getIndustrialServices();
 
         // Combine and transform both sets of URLs
         return [
